8841f4e0d9d688366c811f4f70b0ed56dd870d9f
hzzhaoxinhui <hzzhaoxinhui@corp.netease.com>
2017-08-01 10:08:29 +0800

    Update dockerfile

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const CDP = require('chrome-remote-interface');
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async ctx => {
+    ctx.body = await capture(ctx.request.query.url);
+});
+
+app.listen(3000);
+console.log(`Koa on port: 3000`);
+
+const capture = function (url) {
+    return new Promise((resolve, reject) => {
+        CDP(async (client) => {
+            const { Page } = client;
+
+            await Page.enable();
+            await Page.navigate({ url: url});
+            Page.loadEventFired(async () => {
+                setTimeout(async () => {
+                    const { data } = await Page.captureScreenshot();
+                    console.log(Buffer.from(data, 'base64'))
+                    resolve(Buffer.from(data, 'base64'));
+                }, 0);
+            });
+        }).on('error', (err) => {
+            console.error(err);
+            reject(err);
+        });
+    })
+}
