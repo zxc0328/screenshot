7d1cdca22de87d14ba77b8bbdb00577842356894
hzzhaoxinhui <hzzhaoxinhui@corp.netease.com>
2017-08-01 16:10:03 +0800

    final

--- src/index.js
+++ src/index.js
@@ -3,28 +3,28 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use(async ctx => {
-    ctx.body = await capture(ctx.request.query.url);
+  ctx.body = await capture(ctx.request.query.url);
 });
 
 app.listen(3000);
 console.log(`Koa on port: 3000`);
 
 const capture = function (url) {
-    return new Promise((resolve, reject) => {
-        CDP({ host: "chrome" }, async (client) => {
-            const { Page } = client;
-
-            await Page.enable();
-            await Page.navigate({ url: url });
-            Page.loadEventFired(async () => {
-                const { data } = await Page.captureScreenshot();
-                console.log(Buffer.from(data, 'base64'));
-                resolve(Buffer.from(data, 'base64'));
-                client.close();
-            });
-        }).on('error', (err) => {
-            console.error(err);
-            reject(err);
-        });
-    })
+  return new Promise((resolve, reject) => {
+    CDP.New({ host: "chrome", url: url }).then((target) => {
+      return CDP({ target });
+    }).then(async (client) => {
+      const { Page } = client;
+      await Page.enable();
+      await Page.loadEventFired();
+      const { data } = await Page.captureScreenshot();
+      console.log(Buffer.from(data, 'base64'))
+      resolve(Buffer.from(data, 'base64'));
+      const id = client.target.id;
+      client.close();
+      CDP.Close({id});
+    }).catch((err) => {
+      console.error(err);
+    });
+  })
 }
